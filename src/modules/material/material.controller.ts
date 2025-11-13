43eaa7de2c2f71282f0112fbdfeeda00bf8d1b52
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-11-13 17:06:27 +0330

    feat : update material

--- src/modules/material/material.controller.ts
+++ src/modules/material/material.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { MaterialService } from './material.service';
 import { ApiConsumes } from '@nestjs/swagger';
 import { CreateMaterialDto } from './dto/create-material.dto';
+import { UpdateMaterialDto } from './dto/update-material.dto';
 
 @Controller('material')
 export class MaterialController {
@@ -21,6 +22,12 @@ export class MaterialController {
   @Get(':id')
   findOneById(@Param('id') id: string) {
     return this.materialService.findOneById(+id);
+  }
+
+  @Patch(':id')
+  @ApiConsumes('multipart/form-data')
+  update(@Param('id') id: string, @Body() updateMaterialDto: UpdateMaterialDto) {
+    return this.materialService.update(+id, updateMaterialDto);
   }
 
   @Delete(':id')
